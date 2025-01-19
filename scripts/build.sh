21fd04b043c00caa422869719027d4924a716f93
Laplace825 <laplace_yongle_he@163.com>
2025-01-20 00:17:15 +0800

    link openmp

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,6 +13,6 @@ fi
 cmake -S . -B build -G Ninja \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-  -DCMAKE_CXX_FLAGS="-std=c++2c -stdlib=libc++ -Wall -Wextra -Werror -Wno-unused-variable -O3"
+  -DCMAKE_CXX_FLAGS="-std=c++2c -stdlib=libc++ -Wall -Wextra -Werror -Wno-unused-variable -O3 -fopenmp"
 
 ninja -C build -j 8 -v
